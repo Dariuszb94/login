a32d736d440500b71df3e395803433795092fd85
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-04 21:22:43 +0100

    get user

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 function Profile() {
+  const user = useSelector((state) => state.user);
   return (
     <div>
       <h1>Profile Page</h1>
