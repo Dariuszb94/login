649870633e728f2c4c8c98a5257ef4f94fd76782
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-05 11:17:00 +0100

    login action

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import { useDispatch } from "react-redux";
+import { login } from "../features/user";
 function Login() {
+  const dispatch = useDispatch();
   return (
     <div>
-      <button>Login</button>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "", age: 0, email: "" }));
+        }}
+      >
+        Login
+      </button>
     </div>
   );
 }
--- src/features/user.js
+++ src/features/user.js
@@ -9,4 +9,5 @@ export const userSlice = createSlice({
     },
   },
 });
+export const { login } = userSlice.actions;
 export default userSlice.reducer;
